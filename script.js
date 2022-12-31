51ebed16ea112199953787b7ce3477223f40696c
cameo choquer <cjchoquer@gmail.com>
2022-12-31 13:45:56 +0000

    add in multiple inputs

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+let y = document.querySelectorAll("div.colour")
 let x = document.getElementById("colourPicker");
 
+let picker = [...x];
+let palette = [...y];
+console.log(picker);
+
 setInterval(()=>{
-    let colour = x.value;
+    let colour = picker.value;
     document.getElementById("p1Text").innerHTML = colour;
     document.getElementById("p1").style.backgroundColor = colour;
   },200);
