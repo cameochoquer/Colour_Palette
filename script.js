832951178777a37f5c7e30597267f75315a25b04
cameo choquer <cjchoquer@gmail.com>
2023-01-04 21:25:08 +0000

    added random colour generator function

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ let picker = [...colourPicker];
 let palette = [...paletteBackground];
 let texts = [...text];
 
+function randomColours(){
+    palette.forEach((color) => {
+        let hexCode = '#' + Math.random().toString(16).substring(2, 8);
+        color.style.backgroundColor = hexCode;
+        color.innerHTML = hexCode
+    });
+};
+
+randomColours();
