5028a047c607bf38bd6fdbca59ad1cbf27c42363
cameo choquer <cjchoquer@gmail.com>
2023-01-10 17:00:20 +0000

    the function works! alas!

--- script.js
+++ script.js
@@ -1,31 +1,67 @@
-// let paletteBackground = document.querySelectorAll("div.colour");
-// let text = document.querySelectorAll("p.hexText")
+//the below is what I know actually works 
 
-// let palette = [...paletteBackground];
-// let texts = [...text];
 
-let button = document.getElementById("#generateColour")
-
- // get the color picker elements
- let colourPickers = document.querySelectorAll("#colourPicker");
- let picker = [...colourPickers];
 
  function randomColours(){
-    picker.forEach((index) => {
+    let button = document.getElementById("#generateColour")
+
+ // get the color picker elements
+ let colourPickers = document.querySelectorAll("#colourPicker")
+ let hexValue = document.querySelectorAll("#colourPicker").value;
+ let picker = [...colourPickers];
+
+    picker.forEach((element, index) => {
         let hexCode = '#' + Math.random().toString(16).substring(2, 8);
-        index.value = hexCode;
+        element.value = hexCode;
+        const bigBox = document.getElementById(`p${index + 1}`);
+        bigBox.style.background = hexCode;
     });
+
+    colourPickers.forEach(function (colourPicker) {
+        colourPicker.addEventListener('input', function() {
+          var color = this.value;
+          var colorBox = this.parentNode;
+          console.log(colorBox)
+          colorBox.style.backgroundColor = color;
+
+        });
+      });
 };
 
+
+//or this? 
+
+// function changeDivBgColors() {
+//     const colorPicker = document.querySelector("#color-picker");
+//     const hexValue = colorPicker.value;
+//     const divElements = document.querySelectorAll("div");
+  
+//     divElements.forEach((div) => {
+//       div.style.backgroundColor = hexValue;
+//     });
+//   }
+
+//   const colorPicker = document.querySelector("#color-picker");
+// colorPicker.addEventListener("change", changeDivBgColors);
+
+
+
+// forgotten what the below does....//
+// colourPickers.forEach(function (colourPicker) {
+//     colourPicker.addEventListener('change', function() {
+//       var color = this.value;
+//       var colorBox = this.parentNode;
+//       colorBox.style.backgroundColor = color;
+//     });
  // set the background color of the div element when the corresponding color picker value changes
 
- colourPickers.forEach(function hello(colourPicker) {
-   colourPicker.addEventListener('change', function() {
-     var color = this.value;
-     var colorBox = this.parentNode;
-     colorBox.style.backgroundColor = color;
-   });
- });
+//  colourPickers.forEach(function hello(colourPicker) {
+//    colourPicker.addEventListener('change', function() {
+//      var color = this.value;
+//      var colorBox = this.parentNode;
+//      colorBox.style.backgroundColor = color;
+//    });
+//  });
 
 
 
